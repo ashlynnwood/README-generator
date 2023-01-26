8ddeab757a59da943d0f7d05843a9f8ed1765500
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-01-25 18:28:13 -0600

    Fixed license func

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,28 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Create a function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
-  return `[GitHub license](https://img.shields.io/badge/License-${license}-blue)`
+  // If there is no license, return an empty string
+  if (!license) {
+    return "";
+  } else {
+    return `![GitHub license](https://img.shields.io/badge/License-${license}-blue)`
+}
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Create a function that returns the license link
+function renderLicenseLink(license) {
+  // If there is no license, return an empty string
+  if (!license) {
+    return "";
+  } else{
 
-// TODO: Create a function that returns the license section of README
+  }
+}
+
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
